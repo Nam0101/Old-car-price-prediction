bc50db197ebd0fd55c3b5e5bfe9415121395edf3
Nguyen Van Nam <nam.nv205106@gmail.com>
2023-06-09 01:36:58 +0700

    a

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Project\ML\Old-car-price-prediction\venv
+set VIRTUAL_ENV=D:\2022.2\MLProject\Old-car-price-prediction\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
